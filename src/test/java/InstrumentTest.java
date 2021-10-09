30f0cd3ba9714c89ffb5a7b7daef117afca2c0fc
DSoutter <duncan.soutter@gmail.com>
2021-10-09 14:28:44 +0100

    instrument tests passing

--- src/test/java/InstrumentTest.java
+++ src/test/java/InstrumentTest.java
@@ -1,7 +1,27 @@
+import inventory.instruments.Instrument;
+import inventory.instruments.Piano;
+import inventory.instruments.Type;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class InstrumentTest {
+
+    Instrument instrument;
+
+    @Before
+    public void before(){
+        instrument = new Piano(400.00, 560.00, Type.PIANO, "Wood and metal and others", 88);
+    }
+
+    @Test
+    public void pianoHasMarkup(){
+        assertEquals(160.0, instrument.calculateMarkup(), 0.001);
+    }
+
+    @Test
+    public void pianoHasType(){
+        assertEquals("Clang", instrument.playMusic());
+    }
 }
