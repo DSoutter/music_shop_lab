7fe8e1265c16df24ed68290bfb0908ce93aad81b
DSoutter <duncan.soutter@gmail.com>
2021-10-09 14:56:18 +0100

    shop has inventory of ISell

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,18 @@
+import inventory.ISell;
+import inventory.Inventory;
+
+import java.util.ArrayList;
+
 public class Shop {
+
+    private ArrayList<ISell> inventoryArrayList;
+
+    public Shop(ArrayList<ISell> inventoryArrayList) {
+        this.inventoryArrayList = inventoryArrayList;
+    }
+
+    public int getInventoryArrayListSize() {
+        return inventoryArrayList.size();
+    }
+
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,7 +1,43 @@
+import inventory.ISell;
+import inventory.Inventory;
+import inventory.accessories.Accessory;
+import inventory.instruments.Guitar;
+import inventory.instruments.Piano;
+import inventory.instruments.Type;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 public class ShopTest {
+
+    Shop shop;
+    ArrayList<ISell> inventory;
+    Accessory drumSticks;
+    Piano piano;
+    Guitar guitar;
+
+
+    @Before
+    public void before(){
+        inventory = new ArrayList<>();
+        drumSticks = new Accessory(10.0, 14.99, "Drum Sticks", "Used for the drums");
+        guitar = new Guitar(80.00, 100.00, Type.STRING, "Wood", 6);
+        piano = new Piano(400.00, 560.00, Type.PIANO, "Wood and metal and others", 88);
+
+        inventory.add(drumSticks);
+        inventory.add(guitar);
+        inventory.add(guitar);
+        inventory.add(piano);
+        inventory.add(piano);
+
+        shop = new Shop(inventory);
+    }
+
+    @Test
+    public void inventoryHasSize5(){
+        assertEquals(5, shop.getInventoryArrayListSize());
+    }
 }
